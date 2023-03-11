ea96b88292143c94ce54720ec2637aadea6e4bdf
OscarPOstos <theholderhaizaki@gmail.com>
2023-03-11 17:33:49 +0100

    Detalles del backend

--- padook/Backend/recollect_cars/recollect_cars.py
+++ padook/Backend/recollect_cars/recollect_cars.py
@@ -3,7 +3,7 @@ import pymongo
 
 def get_collection():
     # Crea una conexión con la base de datos MongoDB
-    client = pymongo.MongoClient("mongodb+srv://oportnersaladillo:Y1Qnv81Dvoi7pIwY@cluster0.sdgcsdz.mongodb.net/test")
+    client = pymongo.MongoClient("No te voy a dar el link al mongoDB atlas >:)")
 
     # Selecciona una base de datos específica
     database = client["Coches"]
